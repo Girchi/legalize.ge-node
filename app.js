342cc40ee1d5f62fd8bcf7b89feb0fc0dccdf403
Nika <nabakhtevelin@gmail.com>
2021-07-14 13:57:31 +0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 const port = 3000;
 const hostname = '127.0.0.1';
-
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
+
 
 app.set("view engine", "pug");
 
