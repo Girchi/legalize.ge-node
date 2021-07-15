e312a1d79e9e8fb3e0160562e0ba6022f7315d69
X-Dev <girchi.com>
2021-07-15 16:53:53 +0400

    cardtopngworks
    	new file:   assets/cards/user-1/back.jpg
    	deleted:    assets/cards/user-1/back.png
    	new file:   assets/cards/user-1/front.jpg
    	deleted:    assets/cards/user-1/front.png
    	new file:   assets/cards/user-10/back.jpg
    	new file:   assets/cards/user-10/front.jpg
    	deleted:    assets/cards/user-2/back.PNG
    	new file:   assets/cards/user-2/back.jpg
    	deleted:    assets/cards/user-2/front.PNG
    	new file:   assets/cards/user-2/front.jpg
    	deleted:    assets/cards/user-3/back.PNG
    	new file:   assets/cards/user-3/back.jpg
    	deleted:    assets/cards/user-3/front.PNG
    	new file:   assets/cards/user-3/front.jpg
    	deleted:    assets/cards/user-4/back.PNG
    	new file:   assets/cards/user-4/back.jpg
    	deleted:    assets/cards/user-4/front.PNG
    	new file:   assets/cards/user-4/front.jpg
    	deleted:    assets/cards/user-5/back.PNG
    	new file:   assets/cards/user-5/back.jpg
    	deleted:    assets/cards/user-5/front.PNG
    	new file:   assets/cards/user-5/front.jpg
    	new file:   assets/cards/user-6/back.jpg
    	new file:   assets/cards/user-6/front.jpg
    	new file:   assets/cards/user-7/back.jpg
    	new file:   assets/cards/user-7/front.jpg
    	new file:   assets/cards/user-8/back.jpg
    	new file:   assets/cards/user-8/front.jpg
    	new file:   assets/cards/user-9/back.jpg
    	new file:   assets/cards/user-9/front.jpg
    	new file:   assets/js/generateBackCardTemplate.js
    	new file:   assets/js/generateFrontCardTemplate.js
    	new file:   assets/legalize-qr.png
    	modified:   assets/pdf/1.pdf
    	modified:   cardtopng.js
    	modified:   generatepdf.js
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    users.json

--- generatepdf.js
+++ generatepdf.js
@@ -20,17 +20,17 @@ async function embedImages() {
 
     let counterY = page.getHeight() - 177;
 
-    for(let i = 1; i < 6; i++){
+    for(let i = 6; i < 11; i++){
 
-        const pngUrl = `http://127.0.0.1:3000/assets/cards/user-${i}/front.png`
-        const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
-        const pngImage = await pdfDoc.embedPng(pngImageBytes)
-        const pngDims = pngImage.scale(0.374)
-        page.drawImage(pngImage, {
-            x: 29 ,
-            y: counterY,
-            width: pngDims.width,
-            height: pngDims.height,
+        const jpgUrl = `http://127.0.0.1:3000/assets/cards/user-${i}/front.jpg`
+        const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+        const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+        const jpgDims = jpgImage.scale(0.405)
+        page.drawImage(jpgImage, {
+        x: 29,
+        y: counterY,
+        width: jpgDims.width + 2,
+        height: jpgDims.height + 1,
         })
 
         counterY -= 162.5;
@@ -38,18 +38,20 @@ async function embedImages() {
 
     counterY = page.getHeight() - 177;
 
-    for(let i = 1; i < 6; i++){
+    for(let i = 6; i < 11; i++){
 
-        const pngUrl = `http://127.0.0.1:3000/assets/cards/user-${i}/back.png`
-        const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
-        const pngImage = await pdfDoc.embedPng(pngImageBytes)
-        const pngDims = pngImage.scale(0.374)
-        page.drawImage(pngImage, {
-            x: 309.5,
-            y: counterY,
-            width: pngDims.width,
-            height: pngDims.height,
+
+        const jpgUrl = `http://127.0.0.1:3000/assets/cards/user-${i}/back.jpg`
+        const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+        const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+        const jpgDims = jpgImage.scale(0.405)
+        page.drawImage(jpgImage, {
+        x: 309.5,
+        y: counterY,
+        width: jpgDims.width + 2,
+        height: jpgDims.height + 1,
         })
+
         counterY -= 162.5;
     }
 
