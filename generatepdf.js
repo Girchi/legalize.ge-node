6022fd80f3353b2ce77cbee1a6057ff164df6232
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2021-07-16 14:51:59 +0400

    pdf-generation

--- generatepdf.js
+++ generatepdf.js
@@ -1,16 +1,20 @@
 import { PDFDocument } from 'pdf-lib'
 import fs from 'fs'
 import fetch from 'node-fetch'
+import * as fss from 'fs';
 
-// --CREATE PDF FILES--
-
+// --Generate PDFs--
 async function embedImages() {
-    const jpgUrl = 'http://127.0.0.1:3000/assets/cards/bg.jpg'
-    const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+
+    let jpgUrl, jpgImageBytes, jpgImage, jpgDims;
     const pdfDoc = await PDFDocument.create()
-    const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
-    const jpgDims = jpgImage.scale(0.24)
     const page = pdfDoc.addPage()
+
+    // Add background
+    jpgUrl = 'http://127.0.0.1:3000/generate/card-imgs/other/bg.jpg'
+    jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+    jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+    jpgDims = jpgImage.scale(0.24)
     page.drawImage(jpgImage, {
     x: 0,
     y: page.getHeight() - jpgDims.height,
@@ -18,55 +22,60 @@ async function embedImages() {
     height: jpgDims.height,
     })
 
-    let counterY = page.getHeight() - 177;
+    // Fill with user card images
+    let positionY = page.getHeight() - 177;
 
-    for(let i = 6; i < 11; i++){
+    let imgCounter=fss.readdirSync('generate/card-imgs').length - 1
+    let pdfCounter=fss.readdirSync('generate/pdf').length
 
-        const jpgUrl = `http://127.0.0.1:3000/assets/cards/user-${i}/front.jpg`
-        const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
-        const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
-        const jpgDims = jpgImage.scale(0.405)
+    if(parseInt(imgCounter / 10) > pdfCounter){
+
+    for(let i = pdfCounter * 5; i < pdfCounter * 5 + 5; i++){
+
+        // Card back side
+        jpgUrl = `http://127.0.0.1:3000/generate/card-imgs/${i}-front.jpg`
+        jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+        jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+        jpgDims = jpgImage.scale(0.405)
         page.drawImage(jpgImage, {
         x: 29,
-        y: counterY,
+        y: positionY,
         width: jpgDims.width + 2,
         height: jpgDims.height + 1,
         })
 
-        counterY -= 162.5;
-    }
-
-    counterY = page.getHeight() - 177;
-
-    for(let i = 6; i < 11; i++){
-
-
-        const jpgUrl = `http://127.0.0.1:3000/assets/cards/user-${i}/back.jpg`
-        const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
-        const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
-        const jpgDims = jpgImage.scale(0.405)
+        // Card front side
+        jpgUrl = `http://127.0.0.1:3000/generate/card-imgs/${i}-back.jpg`
+        jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
+        jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+        jpgDims = jpgImage.scale(0.405)
         page.drawImage(jpgImage, {
         x: 309.5,
-        y: counterY,
+        y: positionY,
         width: jpgDims.width + 2,
         height: jpgDims.height + 1,
         })
 
-        counterY -= 162.5;
+        // Y position changes when one image set
+        positionY -= 162.5;
     }
 
     const pdfBytes = await pdfDoc.save()
     console.log("After Save")
-
-   
 
     var callback = (err) => {
         if (err) throw err;
         console.log('It\'s saved!');
       }
 
-       // Buffer
-    fs.writeFile('./assets/pdf/test-cards.pdf', pdfBytes, callback);
+    // Saves into local folder
+    fs.writeFile(`./generate/pdf/${pdfCounter}.pdf`, pdfBytes, callback);
+    } else {
+        console.log('All pdf created based on provided images')
+        console.log('There must be 5 unused image to set in pdf')
+    }
+
 }
 
 embedImages();
+
