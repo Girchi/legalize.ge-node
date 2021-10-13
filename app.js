9b398c0e3e545dd975c561b4135a7c7412f5b674
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2021-10-13 15:55:52 +0400

    clean code

--- app.js
+++ app.js
@@ -94,155 +94,160 @@ app.get('/create-card', (req, res) => {
 });
 
 // User verify and save data
-app.post('/create-card', [urlencodedParser, upload.single('image')], async (req, res) => {
-	try {
-		const drupalResponse = await axiosInstance.get('/jsonapi', { cache: 'no-cache', headers: { Authorization: req.body.token } });
+app.post( "/create-card", [urlencodedParser, upload.single("image")], async (req, res) => {
+  try {
+    const drupalResponse = await axiosInstance.get('/jsonapi/', { cache: 'no-cache', headers: { 'Authorization': req.body.token } } );
 
-		// If user logged correctly
-		if (drupalResponse.data.meta) {
-			if (req.body.other_statuses == null) req.body.other_statuses = [];
-			if (typeof req.body.other_statuses !== 'object') req.body.other_statuses = [req.body.other_statuses];
+    // If user logged correctly
+    if(drupalResponse.data.meta){
+      
+      if(req.body.other_statuses == null) req.body.other_statuses = [];
+      if(typeof req.body.other_statuses !== "object") req.body.other_statuses = [req.body.other_statuses];
 
-			const fullName = convertLetters(req.body.name, 'geo').trim().split(/[ ]+/gi);
-			const idNumTaken = fs.existsSync(`./database/${req.body.id_number}.json`);
+      const fullName = convertLetters(req.body.name, 'geo').trim().split(/[ ]+/gi);
+      const idNumTaken = fs.existsSync(`./database/${req.body.id_number}.json`);
 
-			req.body.drupal_id = drupalResponse.data.meta.links.me.meta.id;
-			req.body.img = `/assets/img/users-images/${req.file.originalname}`;
-			req.body.name = fullName.join(' ');
-			req.body.registration = new Date().toISOString().slice(0, 10);
+      req.body.drupal_id = drupalResponse.data.meta.links.me.meta.id;
+      req.body.img = `/assets/img/users-images/${req.file.originalname}`;
+      req.body.name = fullName.join(" ");
+      req.body.registration = new Date().toISOString().slice(0, 10);
+     
+      async function userSaveToDrupal(details, firstName, lastName) {
+        // Save user into drupal base
+        try {
+          const config = {
+            headers: {
+              "Content-Type": "application/vnd.api+json",
+              "Accept": "application/vnd.api+json",
+              'Authorization': details.token,
+            }
+          };
+          const body = {
+            data: {
+                type: "user--user",
+                id: details.drupal_id,
+                attributes: {
+                    field_first_name: firstName,
+                    field_last_name: lastName,
+                    field_personal_id: details.id_number,
+                    field_date_of_birth: details.birth_date,
+                }
+            }
+          };
+          await axiosInstance.patch(`/jsonapi/user/user/${details.drupal_id}`, body, config );
+        } catch (err) {
+          console.log(err)
+        }
+      }
 
-			async function userSaveToDrupal(details, firstName, lastName) {
-				// Save user into drupal base
-				try {
-					const config = {
-						headers: {
-							'Content-Type': 'application/vnd.api+json',
-							Accept: 'application/vnd.api+json',
-							Authorization: details.token,
-						},
-					};
-					const body = {
-						data: {
-							type: 'user--user',
-							id: details.drupal_id,
-							attributes: {
-								field_first_name: firstName,
-								field_last_name: lastName,
-								field_personal_id: details.id_number,
-								field_date_of_birth: details.birth_date,
-							},
-						},
-					};
-					await axiosInstance.patch(`/jsonapi/user/user/${details.drupal_id}`, body, config);
-				} catch (err) {
-					console.log(err);
-				}
-			}
+      async function userSaveToServer(details) {
+        // Save user to Server
+        try {
+          // Optimize image before save
+          await sharp(req.file.buffer).rotate()
+          .resize({width: 1000})
+          .toFile(`./assets/img/users-images/${req.file.originalname}`);
 
-			async function userSaveToServer(details) {
-				// Save user to Server
-				try {
-					// Optimize image before save
-					await sharp(req.file.buffer).rotate().resize({ width: 1000 }).toFile(`./assets/img/users-images/${req.file.originalname}`);
+          delete details.token;
+          fs.writeFileSync(`./database/${details.id_number}.json`, JSON.stringify(details), err => { if(err) console.log(err) })
+          res.redirect(`/user/${details.id_number}`);
 
-					delete details.token;
-					fs.writeFileSync(`./database/${details.id_number}.json`, JSON.stringify(details), (err) => {
-						if (err) console.log(err);
-					});
-					res.redirect(`/user/${details.id_number}`);
+          // Create card image
+          cardtoimg(details)
+        } catch (err) {
+          console.log(err)
+        }
+      }
 
-					// Create card image
-					cardtoimg(details);
-				} catch (err) {
-					console.log(err);
-				}
-			}
+      // If id number already exists on server
+      if(idNumTaken){
 
-			// If id number already exists on server
-			if (idNumTaken) {
-				const userData = JSON.parse(fs.readFileSync(`./database/${req.body.id_number}.json`, 'utf8'));
-				if (userData.drupal_id === req.body.drupal_id) {
-					req.body.card_number = userData.card_number;
-					userSaveToDrupal(req.body, fullName[0], fullName[fullName.length - 1]);
-					userSaveToServer(req.body);
-				} else {
-					console.log('id number already used');
-					res.redirect('/create-card');
-				}
-			} else {
-				// Delete past user info file on server
-				fs.readdirSync('./database').filter((userJSON) => {
-					userJSON = JSON.parse(fs.readFileSync(`./database/${userJSON}`, 'utf8'));
-					if (userJSON.drupal_id === req.body.drupal_id) fs.unlink(`./database/${userJSON.id_number}.json`, (err) => console.log(err));
-				});
-				req.body.card_number = nextCardNum();
-				userSaveToDrupal(req.body, fullName[0], fullName[fullName.length - 1]);
-				userSaveToServer(req.body);
-			}
-		} else {
-			// If user is not logged
-			res.redirect('/create-card');
-		}
-	} catch (err) {
-		// If token is not valid
-		if (err.response.status === 401) {
-			console.log('Token is not vaild');
-			res.redirect('/?param1=expired');
-		} else {
-			// if drupal side problems or else
-			console.log(err.message);
-			res.redirect('/create-card');
-		}
-	}
+        const userData = JSON.parse(fs.readFileSync(`./database/${req.body.id_number}.json`, 'utf8'));
+        if(userData.drupal_id === req.body.drupal_id){
+          req.body.card_number = userData.card_number;
+          userSaveToDrupal(req.body, fullName[0], fullName[fullName.length - 1]);
+          userSaveToServer(req.body);
+        } else {
+          console.log('id number already used')
+          res.redirect('/create-card')
+        }
+  
+      } else {
+        // Delete past user info file on server
+        fs.readdirSync("./database").filter(userJSON => {
+          userJSON = JSON.parse(fs.readFileSync(`./database/${userJSON}`, 'utf8'));
+          if(userJSON.drupal_id === req.body.drupal_id) fs.unlink(`./database/${userJSON.id_number}.json`, (err) => console.log(err));
+        });
+        req.body.card_number = nextCardNum();
+        userSaveToDrupal(req.body, fullName[0], fullName[fullName.length - 1]);
+        userSaveToServer(req.body);
+      }
+
+    } else {
+    // If user is not logged
+      res.redirect('/create-card')
+    }
+    
+  } catch (err) {
+    // If token is not valid
+    if(err.response.status === 401){
+      console.log('Token is not vaild')
+      res.redirect('/?param1=expired')
+    } else {
+      // if drupal side problems or else
+      console.log(err.message)
+      res.redirect('/create-card')
+    }
+  }
 });
 
 // User Authorization
-app.get('/authorization/:authType/:token&:expirationTime&:userID', async (req, res) => {
-	const formData = new FormData();
+app.get("/authorization/:authType/:token&:expirationTime&:userID", async (req, res) => {
 
-	switch (req.params.authType) {
-		case 'fb':
-			formData.append('grant_type', 'facebook_login_grant');
-			formData.append('facebook_access_token', req.params.token);
-			formData.append('facebook_user_id', req.params.userID);
-			formData.append('facebook_token_expires_in', req.params.expirationTime);
-			break;
-		case 'refresh':
-			formData.append('grant_type', 'refresh_token');
-			formData.append('refresh_token', req.params.token);
-			formData.append('client_id', process.env.CLIENT_ID);
-			formData.append('client_secret', process.env.SECRET_KEY);
-			break;
-		default:
-			console.log(req.params.authType);
-	}
+  const formData = new FormData()
 
-	formData.append('client_id', process.env.CLIENT_ID);
-	formData.append('client_secret', process.env.SECRET_KEY);
+  switch (req.params.authType) {
+    case 'fb':
+      formData.append("grant_type", "facebook_login_grant");
+      formData.append("facebook_access_token", req.params.token);
+      formData.append("facebook_user_id", req.params.userID);
+      formData.append("facebook_token_expires_in", req.params.expirationTime);
+      break;
+    case 'refresh':
+      formData.append("grant_type", "refresh_token");
+      formData.append("refresh_token", req.params.token);
+      break;
+    default:
+      console.log(req.params.authType);
+  } 
 
-	try {
-		// Get user information
-		const oauthTokens = await axiosInstance.post('/oauth/token', formData, { headers: formData.getHeaders() });
-		const token = `Bearer ${oauthTokens.data.access_token}`;
-		const refreshToken = oauthTokens.data.refresh_token;
-		const drupalResponse = await axiosInstance.get('/jsonapi', { cache: 'no-cache', headers: { Authorization: token } });
+  formData.append("client_id", process.env.CLIENT_ID);
+  formData.append("client_secret", process.env.SECRET_KEY);
 
-		const drupalID = drupalResponse.data.meta.links.me.meta.id;
+  try {
+    // Get user information
+    const oauthTokens = await axiosInstance.post('/oauth/token', formData, { headers: formData.getHeaders() } );
+    const token = `Bearer ${oauthTokens.data.access_token}`;
+    const refreshToken = oauthTokens.data.refresh_token;
+    
+    const drupalResponse = await axiosInstance.get('/jsonapi/', { cache: 'no-cache', headers: { 'Authorization': token } } );
+    const drupalID = drupalResponse.data.meta.links.me.meta.id
 
-		const userResponse = await axiosInstance.get(`/jsonapi/user/user/${drupalID}`, { cache: 'no-cache', headers: { Authorization: token } });
-		const userPictureResponse = await axiosInstance.get(`/jsonapi/user/user/${drupalID}/user_picture`, { cache: 'no-cache', headers: { Authorization: token } });
+    const userResponse = await axiosInstance.get(`/jsonapi/user/user/${drupalID}`, { cache: 'no-cache', headers: { 'Authorization': token } } );
+    const userPictureResponse = await axiosInstance.get(`/jsonapi/user/user/${drupalID}/user_picture`, { cache: 'no-cache', headers: { 'Authorization': token } } );
 
-		const userLoginName = userResponse.data.data.attributes.name;
-		const userFirstName = userResponse.data.data.attributes.field_first_name;
-		const userLastName = userResponse.data.data.attributes.field_last_name;
-		const userPicture = userPictureResponse.data.data ? process.env.DRUPAL_DOMAIN + userPictureResponse.data.data.attributes.uri.url : `/assets/img/avatar.png`;
+    const userLoginName = userResponse.data.data.attributes.name
+    const userFirstName = userResponse.data.data.attributes.field_first_name
+    const userLastName = userResponse.data.data.attributes.field_last_name
+    const userPicture = userPictureResponse.data.data ? process.env.DRUPAL_DOMAIN + userPictureResponse.data.data.attributes.uri.url : `/assets/img/avatar.png`;
 
-		const localStore = { token, refreshToken, drupalID, userPicture, userFirstName, userLastName, userLoginName };
-		res.send({ localStore });
-	} catch (err) {
-		console.log(`error: ${err}`);
-		res.redirect('/');
-	}
+    const localStore = { token, refreshToken, drupalID, userPicture, userFirstName, userLastName, userLoginName }
+    res.send({localStore})   
+
+  } catch (error) {
+    console.log(error.message)
+    res.send(error.message)
+  }
 });
 
 // Countitution Page
